326df5e60ce2b9ec743b2bbe7b1c5c4bbf83b4b9
Tobias Walle <tobias.walle@gmail.com>
2017-09-02 21:59:26 +0200

    Add several counters and reset button

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { div, DOMSource, makeDOMDriver, VNode } from '@cycle/dom';
+import { button, div, DOMSource, makeDOMDriver, VNode } from '@cycle/dom';
 import { run } from '@cycle/run';
 import xs, { Stream } from 'xstream';
+import { logDriver } from './log.driver';
 import './polyfills';
-import { logDriver, LogSink } from './log.driver';
 
 interface Sources {
   DOM: DOMSource;
@@ -10,21 +10,60 @@ interface Sources {
 
 interface Sinks {
   DOM: Stream<VNode>;
-  log: LogSink;
+}
+
+function countToMsg(i: number): string {
+  return `Count: ${i}`;
+}
+
+function msgToDom(msg: string): VNode {
+  return div(msg);
 }
 
 function main(sources: Sources): Sinks {
-  const count$ = xs.periodic(1000).map(i => `Count: ${i}`);
+  const count$ = sources.DOM.select('.button')
+    .events('click')
+    .map(() => xs.periodic(1000))
+    .startWith(xs.periodic(1000))
+    .flatten();
+
+  const doubleCount$ = count$.map(i => i * 2);
+  const tripleCount$ = count$.map(i => i * 3);
+  const power2Count$ = count$.map(i => i ** 2);
+
+  const msg$ = xs.combine(
+    doubleCount$.map(countToMsg),
+    tripleCount$.map(countToMsg),
+    power2Count$.map(countToMsg),
+  );
   return {
-    log: count$,
-    DOM: count$.map(msg => div(msg)),
+    DOM: msg$
+      .map(counts => counts.map(msgToDom))
+      .map(messages => div(
+        [...messages, button('.button', 'Reset')]
+      ))
+    ,
   };
 }
-
 
 const drivers = {
   DOM: makeDOMDriver('#app'),
   log: logDriver,
 };
 
-run(main, drivers);
+function mapObject<T, O extends {[key in keyof O]: T}, R>(obj: O, callback: (e: T, key: string) => R): {[key in keyof O]: R} {
+  return Object.keys(obj)
+    .reduce<{[key in keyof O]: R}>((result, key: keyof O) => {
+      result[key] = callback(obj[key], key);
+      return result;
+    }, {} as any);
+}
+
+function middleware(source: Sources): Sinks {
+  let sinks: Sinks = main(source);
+  console.log(sinks);
+  console.log(source);
+  return sinks;
+}
+
+run(middleware, drivers);
--- src/log.driver.ts
+++ src/log.driver.ts
@@ -2,7 +2,7 @@
 import xs, { Stream } from 'xstream';
 import { Driver } from '@cycle/run';
 
-export type LogSink = Stream<string>;
+export type LogSink = Stream<any>;
 export const logDriver: Driver<LogSink, void> = (sink$, name) => {
   sink$.addListener({
     next: s => console.log(s)
